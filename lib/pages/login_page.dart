afcb517d72bb3dafd6a4b147cf31dad193c7c161
= <=>
2020-10-20 20:28:09 +0700

    Penambahan validasi username dan password

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,6 +6,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  ///Globakey
+  final _keyFrom = GlobalKey<FormState>();
+
   ///variabel controller
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -46,132 +49,152 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.centerLeft,
-            colors: [Colors.lightBlue, Colors.white, ]
-          )
-        ),
-        child: Center(
-          child: ListView(
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: 20),
-                  SizedBox(
-                    height: 30,
-                    child: Text(
-                      "Selamat Datang Kembali!",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
+      body: Form(
+        key: _keyFrom,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.centerLeft,
+                  colors: [
+                Colors.lightBlue,
+                Colors.white,
+              ])),
+          child: Center(
+            child: ListView(
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    SizedBox(
+                      height: 30,
+                      child: Text(
+                        "Selamat Datang Kembali!",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
                     ),
-                  ),
-                  Container(
-                    height: 250,
-                    child: LottieBuilder.asset('assets/computer.json')
-                  ),
-                  TextField(
-                    controller: usernameController,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.supervised_user_circle),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        labelText: "Username",
-                        hintText: "Username"),
-                  ),
-                  SizedBox(height: 16),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: _isHidePassword,
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock_outline),
-                        suffixIcon: GestureDetector(
+                    Container(
+                        height: 250,
+                        child: LottieBuilder.asset('assets/computer.json')),
+                    TextFormField(
+                      controller: usernameController,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please Input Username';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.supervised_user_circle),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          labelText: "Username",
+                          hintText: "Username"),
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Masukan Password Dengan benar';
+                        } else if (value.length > 6) {
+                          return 'Password Terlalu Panjang';
+                        }
+                        return null;
+                      },
+                      controller: passwordController,
+                      obscureText: _isHidePassword,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock_outline),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              _togglePasswordVisibility();
+                            },
+                            child: Icon(
+                              _isHidePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color:
+                                  _isHidePassword ? Colors.grey : Colors.blue,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          labelText: "Password",
+                          hintText: "Password"),
+                    ),
+                    SizedBox(height: 6),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Lupa Password? ",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        Text(
+                          "Click Here",
+                          style: TextStyle(color: Colors.blue, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    RaisedButton(
+                        elevation: 0,
+                        color: Colors.blue,
+                        shape: StadiumBorder(),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        ),
+                        onPressed: () {
+                          if (_keyFrom.currentState.validate()) {
+                            setState(() {
+                              username = usernameController.text;
+                              password = passwordController.text;
+
+                              if (username == "admin" && password == "admin") {
+                                Get.to(MainPage(
+                                  username: username,
+                                  password: password,
+                                ));
+                                toast("Login Success");
+                              } else {
+                                toast('Error');
+                              }
+                            });
+                          }
+                        }),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Belum punya akun?",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        GestureDetector(
                           onTap: () {
-                            _togglePasswordVisibility();
+                            Get.to(RegisterPage());
                           },
-                          child: Icon(
-                            _isHidePassword
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                            color: _isHidePassword ? Colors.grey : Colors.blue,
+                          child: Text(
+                            "Register",
+                            style: TextStyle(color: Colors.blue),
                           ),
                         ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        labelText: "Password",
-                        hintText: "Password"),
-                  ),
-                  SizedBox(height: 6),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Lupa Password? ",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w300),
-                      ),
-                      Text(
-                        "Click Here",
-                        style: TextStyle(color: Colors.blue, fontSize: 12),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  RaisedButton(
-                      elevation: 0,
-                      color: Colors.blue,
-                      shape: StadiumBorder(),
-                      child: Text(
-                        "Login",
-                        style: TextStyle(color: Colors.white, fontSize: 12),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          username = usernameController.text;
-                          password = passwordController.text;
-                          if (username == "admin" && password == "admin") {
-                            Get.to(MainPage(
-                              username: username,
-                              password: password,
-                            ));
-                            toast("Login Success");
-
-                          } else {
-                            toast('Error');
-                          }
-                        });
-                      }),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Belum punya akun?",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Get.to(RegisterPage());
-                        },
-                        child: Text(
-                          "Register",
-                          style: TextStyle(color: Colors.blue),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                ],
-              )
-            ],
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
